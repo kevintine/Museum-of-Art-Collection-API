da2482b8bc9d7c12fbab29f0d6e4ab187ebc3bbf
Kevin <kevin.tranr@outlook.com>
2023-02-27 15:32:33 -0500

    ArtworkCardDetail.js nearly done

--- my-app/components/ArtworkCardDetail.js
+++ my-app/components/ArtworkCardDetail.js
@@ -6,7 +6,7 @@ import useSWR from 'swr'
 // We are using swr in this component to fetch data from the API
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-export default function ArtworkCard({ objectID }) {
+export default function ArtworkCardDetail({ objectID }) {
   const url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`;
   const { data, error } = useSWR(url, fetcher);
   if (error) return <Error statusCode={404}/>;
@@ -14,7 +14,7 @@ export default function ArtworkCard({ objectID }) {
   return (
     <>
         <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={data.primaryImageSmall ? data.primaryImageSmall : "https://via.placeholder.com/375x375.png?text=[+Not+Available+]"} />
+        {data.primaryImage ? <Card.Img variant="top" src={data.primaryImage} /> : null}
         <Card.Body>
             <Card.Title>{data.title ? data.title : "N/A"}</Card.Title>
             <Card.Text>
