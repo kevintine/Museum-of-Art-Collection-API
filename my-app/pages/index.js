03f39d8b82b57d3fffbcb49171049ef2e5001224
Kevin <kevin.tranr@outlook.com>
2023-03-05 22:33:22 -0500

    Added Student #

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB422 – Assignment 4
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Kevin Tran Student ID: 026411140 Date: 2023-03-05
+*
+*
+********************************************************************************/ 
+
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
